e92d9e2aa89dbca63a0123b0c2b0d146a8184c56
Aldric Borders <aldricborders@gmail.com>
2021-02-03 19:03:16 -0700

    reformatted test array syntax

--- src/test/vector.test.js
+++ src/test/vector.test.js
@@ -1,27 +1,29 @@
 const { makeVector3D } = require("../vector");
 
 describe("Create a Vector3D", () => {
-    test("with default parameters", () => {
-        let testVector = makeVector3D();
+  test("with default parameters", () => {
+    let testVector = makeVector3D();
 
-        expect(testVector[0]).toBe(0);
-        expect(testVector[1]).toBe(0);
-        expect(testVector[2]).toBe(0);
+    expect(testVector[0]).toBe(0);
+    expect(testVector[1]).toBe(0);
+    expect(testVector[2]).toBe(0);
 
-        expect(testVector.length).toBe(3);
-    });
+    expect(testVector.length).toBe(3);
+  });
 
-    test("with any passed parameters", () => {
-        let x = Math.floor(Math.random() * 10);
-        let y = Math.floor(Math.random() * 10);
-        let z = Math.floor(Math.random() * 10);
+  test("with any passed parameters", () => {
+    let vectorValues = ([x, y, z] = [
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+    ]);
 
-        let testVector = makeVector3D(x, y, z);
+    let testVector = makeVector3D(...vectorValues);
 
-        expect(testVector[0]).toBe(x);
-        expect(testVector[1]).toBe(y);
-        expect(testVector[2]).toBe(z);
+    expect(testVector[0]).toBe(x);
+    expect(testVector[1]).toBe(y);
+    expect(testVector[2]).toBe(z);
 
-        expect(testVector.length).toBe(3);
-    });
+    expect(testVector.length).toBe(3);
+  });
 });
